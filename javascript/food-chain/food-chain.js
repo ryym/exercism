3aaa10d04aa4e0082a4780ecf042db329ed81178
ryym <ryym.64@gmail.com>
2017-01-30 16:45:35 +0900

    javascript: food-chain: Gather sentences as object

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -6,9 +6,17 @@ const DEFAULT_MIDDLE_CHAIN = [
   ['cow', "I don't know how she swallowed a cow!"],
 ]
 
-const SENTENCES = {
-  ABOUT_SPIDER: 'wriggled and jiggled and tickled inside her.',
-  CLOSING: "I don't know why she swallowed the fly. Perhaps she'll die.",
+const sentences = {
+  opening: (animal) =>
+    `I know an old lady who swallowed a ${animal}.`,
+  closing: () =>
+    "I don't know why she swallowed the fly. Perhaps she'll die.",
+  thatsAll: () =>
+    "She's dead, of course!",
+  swallowing: (catcher, catchee) =>
+    `She swallowed the ${catcher} to catch the ${catchee}`,
+  aboutSpider: () =>
+    'wriggled and jiggled and tickled inside her.',
 }
 
 /**
@@ -23,7 +31,7 @@ module.exports = class FoodChain {
       .concat(middleChain.map(c => c[0]))
       .concat(lastAnimal)
 
-    this.comments = [null, `It ${SENTENCES.ABOUT_SPIDER}`]
+    this.comments = [null, `It ${sentences.aboutSpider()}`]
       .concat(middleChain.map(c => c[1]))
   }
 
@@ -41,7 +49,7 @@ module.exports = class FoodChain {
     }
     const idx = n - 1
     const section = [
-      `I know an old lady who swallowed a ${this.animals[idx]}.`,
+      sentences.opening(this.animals[idx]),
       ...this.composeSection(idx)
     ]
     return section.join('\n') + '\n'
@@ -49,22 +57,22 @@ module.exports = class FoodChain {
 
   composeSection(idx) {
     if (idx === this.animals.length - 1) {
-      return ["She's dead, of course!"]
+      return [sentences.thatsAll()]
     }
     const comment = this.comments[idx]
     const reasons = this.composeReasons(idx)
-    return (comment ? [comment] : []).concat(reasons).concat(SENTENCES.CLOSING)
+    return (comment ? [comment] : []).concat(reasons).concat(sentences.closing())
   }
 
   composeReasons(idx) {
     const reasons = []
     for (let i = idx; i > 0; i--) {
       const [catchee, catcher] = this.animals.slice(i - 1, i + 1)
-      const sentence = [
-        `She swallowed the ${catcher} to catch the ${catchee}`,
-        catchee === 'spider' ? ` that ${SENTENCES.ABOUT_SPIDER}` : '.',
+      const reason = [
+        sentences.swallowing(catcher, catchee),
+        catchee === 'spider' ? ` that ${sentences.aboutSpider()}` : '.',
       ].join('')
-      reasons.push(sentence)
+      reasons.push(reason)
     }
     return reasons
   }

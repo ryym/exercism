766650a23b5823127edacf5cf0ea0d6312c59e90
ryym <ryym.64@gmail.com>
2017-02-07 10:05:48 +0900

    haskell: run-length-encoding: Use concat

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -4,10 +4,10 @@ import Data.Char (isNumber)
 import Data.List (group)
 
 decode :: String -> String
-decode = flatten . decompress
+decode = concat . decompress
 
 encode :: String -> String
-encode = flatten . compress
+encode = concat . compress
 
 decompress :: String -> [String]
 decompress [] = []
@@ -18,11 +18,8 @@ decompress s = (replicate n c) : decompress rest
         n = if null nums then 1 else read nums
 
 compress :: String -> [String]
-compress = concat . (map pairs) . group
+compress = consEach . (map pair) . group
     where
-        concat = foldr (\(n, c) ss -> n:[c]:ss) []
-        pairs g = (count $ length g, head g)
+        consEach = foldr (\(n, c) ss -> n:[c]:ss) []
+        pair g = (count $ length g, head g)
         count n = if n == 1 then "" else show n
-
-flatten :: [[a]] -> [a]
-flatten = foldr (flip $ foldr (:)) []

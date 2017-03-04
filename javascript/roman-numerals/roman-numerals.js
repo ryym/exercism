c5fe55af8fbfee217796d551a34d45b1857c959a
ryym <ryym.64@gmail.com>
2017-03-04 12:29:11 +0900

    javascript: roman-numerals: Simplify version

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,75 +1,34 @@
-// The internal representation of roman numeral units.
-// 1-9: I, V, X | 10-90: X, L, C | ..
-const ONE = 'ROMAN-ONE'
-const FIVE = 'ROMAN-FIVE'
-const TEN = 'ROMAN-TEN'
-
-// repeat(3, 1) -> [1, 1, 1]
-const repeat = (n, value) => {
-  const arr = []
-  for (let i = 0; i < n; i++) {
-    arr.push(value)
-  }
-  return arr
+const makeNumeralPairs = groups => {
+  const pairs = [[1, groups[0][0]]]
+  return groups.reduce((p, [one, five, ten], i) => {
+    const d = Math.pow(10, i)
+    return p.concat([
+      [d * 4, one + five],
+      [d * 5, five],
+      [d * 9, one + ten],
+      [d * 10, ten],
+    ]) 
+  }, pairs)
 }
 
-// 123 -> [1, 2, 3]
-const divideToDigits = (n, nDigits) => {
-  const ds = String(n).split('').map(s => Number(s))
-  return repeat(nDigits - ds.length, 0).concat(ds)
-}
+// [[1000, 'M'], [900, 'CM'], [500, 'D'], ..]
+const numeralPairs = makeNumeralPairs([
+  ['I', 'V', 'X'],
+  ['X', 'L', 'C'],
+  ['C', 'D', 'M'],
+]).reverse()
 
-// 3 -> [ONE, ONE, ONE]
-const digitToUnits = d => {
-  console.assert(0 <= d && d <= 9)
-  if (d === 0) {
-    return []
-  }
-  const nOnes = d % 5
-  return nOnes === 4
-    ? [ONE, d < 5 ? FIVE : TEN]
-    : (d < 5 ? [] : [FIVE]).concat(repeat(nOnes, ONE))
-}
-
-// 3 -> 'III', 4 -> 'IV'
-const convertDigit = (d, symbols) => {
-  const units = digitToUnits(d)
-  return units.map(u => symbols[u] || '').join('')
-}
-
-// [I, V, X, L, C] -> [[X, L, C], [I, V, X]]
-const groupRomanSymbols = (...symbols) => {
-  symbols = symbols.slice()
-  const groups = []
-  for (let i = 1; i < symbols.length; i += 2) {
-    groups.push({
-      [ONE]: symbols[i - 1],
-      [FIVE]: symbols[i],
-      [TEN]: symbols[i + 1],
-    })
-  }
-  return groups.reverse()
-}
-
-// Create a converter which transforms arabic numerals to roman numerals.
-const makeRomanNumConverter = (...romanSymbols) => {
-  const symbolGroups = groupRomanSymbols(...romanSymbols)
-
-  return maybeNumber => {
-    const arabicN = Number(maybeNumber)
-    if (Number.isNaN(arabicN)) {
-      throw new Error(`${maybeNumber} is not a number`)
+const toRoman = arabicN => {
+  const convert = (arabTotal, romanTotal, i) => {
+    const [arab, roman] = numeralPairs[i]
+    if (arabTotal === 0) {
+      return romanTotal
     }
-    const digits = divideToDigits(arabicN, 4)
-    const romanNs = digits.map((d, i) => convertDigit(d, symbolGroups[i]))
-    return romanNs.join('')
+    return arab <= arabTotal
+      ? convert(arabTotal - arab, romanTotal + roman, i)
+      : convert(arabTotal, romanTotal, i + 1)
   }
+  return convert(arabicN, '', 0)
 }
-
-const toRoman = makeRomanNumConverter(
-  'I', 'V', 'X',
-  'L', 'C', 'D',
-  'M', '?', '?'
-)
 
 module.exports = toRoman

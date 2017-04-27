a27a4e2ef1d98153635071dba040c3563f602ef2
ryym <ryym.64@gmail.com>
2017-04-28 08:09:07 +0900

    javascript: perfect-numbers: Improve logic
    The logic was taken from the other answer I commented about.

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -1,21 +1,22 @@
+// REF: http://exercism.io/submissions/a8715bf7f88f4680a46facf65be6fd1d
+
 module.exports = class PerfectNumbers {
   classify(n) {
     if (n < 1) {
       return 'Classification is only possible for natural numbers.'
     }
-
-    const half = Math.floor(n / 2)
-    let total = half
-
-    for (let i = 1; i < half; i++) {
-      if (n % i === 0) {
-        total += i
-        if (total > n) {
-          break
-        }
-      }
-    }
-
+    const total = this.sumFactors(n)
     return total > n ? 'abundant' : (total < n ? 'deficient' : 'perfect')
+  }
+
+  sumFactors(n) {
+    if (n === 1) {
+      return 0
+    }
+    const sroot = Math.ceil(Math.sqrt(n))
+    const nsToSroot = Array.from(Array(sroot).keys())
+    const minFs = nsToSroot.slice(1).filter(i => n % i === 0)
+    const opposites = minFs.slice(1).map(i => n / i === i ? 0 : n / i)
+    return minFs.concat(opposites).reduce((a, b) => a + b)
   }
 }

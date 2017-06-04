342997461c2a0366a99a843da58b22dc854efa7a
ryym <ryym.64@gmail.com>
2017-06-05 00:12:36 +0900

    javascript: strain: Simplify

--- javascript/strain/strain.js
+++ javascript/strain/strain.js
@@ -1,15 +1,14 @@
-const makeFilter = (isTarget) => (items, p) => {
+const keep = (items, p) => {
   const result = []
   for (let i = 0; i < items.length; i++) {
     const item = items[i]
-    if (isTarget(p(item))) {
+    if (p(item)) {
       result.push(item)
     }
   }
   return result
 }
 
-module.exports = {
-  keep: makeFilter(b => b),
-  discard: makeFilter(b => !b),
-}
+const discard = (items, p) => keep(items, t => !p(t))
+
+module.exports = { keep, discard }

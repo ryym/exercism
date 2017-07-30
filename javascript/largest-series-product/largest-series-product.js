422416d253389795d266d0d03eb45266b4e5f454
ryym <ryym.64@gmail.com>
2017-07-30 21:31:24 +0900

    javascript: largest-series-product: Improve var names

--- javascript/largest-series-product/largest-series-product.js
+++ javascript/largest-series-product/largest-series-product.js
@@ -1,24 +1,24 @@
 module.exports = class Series {
   constructor(numbers) {
-    this.xs = numbers.split('').map(Number)
+    this.ns = numbers.split('').map(Number)
   }
 
-  largestProduct(n) {
-    if (typeof n !== 'number' || n < 0) {
+  largestProduct(slice) {
+    if (typeof slice !== 'number' || slice < 0) {
       throw new Error('Invalid input.')
     }
-    const { xs } = this
+    const { ns } = this
 
-    if (xs.length < n) {
+    if (ns.length < slice) {
       throw new Error('Slice size is too big.')
     }
-    if (xs.length === 0 || n === 0) {
+    if (ns.length === 0 || slice === 0) {
       return 1
     }
 
     let max = 0
-    for (let i = 0; i <= xs.length - n; i++) {
-      const prod = xs.slice(i, i + n).reduce((a, b) => a * b)
+    for (let i = 0; i <= ns.length - slice; i++) {
+      const prod = ns.slice(i, i + slice).reduce((a, b) => a * b)
       max = Math.max(max, prod)
     }
     return max

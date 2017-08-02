4a7bb5d41161d2691220f4b252fe011a53d1bb56
ryym <ryym.64@gmail.com>
2017-08-03 08:18:59 +0900

    javascript: pascals-triangle: Extract sub function

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,19 +1,21 @@
+const makeNextRow = (prev) => {
+  const next = []
+
+  next.push(prev[0])
+  for (let i = 1; i < prev.length; i++) {
+    next.push(prev[i - 1] + prev[i])
+  }
+  next.push(prev[prev.length - 1])
+
+  return next
+}
+
 const pascalsTriangle = (nRows, rows = []) => {
   if (nRows <= 0) {
     return rows
   }
-
-  const next = []
-  if (rows.length === 0) {
-    next.push(1)
-  } else {
-    const prev = rows[rows.length - 1]
-    for (let i = 0; i <= prev.length; i++) {
-      const v = (prev[i - 1] || 0) + (prev[i] || 0)
-      next.push(v)
-    }
-  }
-
+  const prev = rows[rows.length - 1]
+  const next = prev ? makeNextRow(prev) : [1]
   return pascalsTriangle(nRows - 1, [...rows, next])
 }
 

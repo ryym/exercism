e3262c57eaabbab47d8947736b535d10f76783d2
ryym <ryym.64@gmail.com>
2017-08-11 10:01:57 +0900

    javascript: sum-of-multiples: Simplify using filter

--- javascript/sum-of-multiples/sum-of-multiples.js
+++ javascript/sum-of-multiples/sum-of-multiples.js
@@ -1,13 +1,9 @@
+// ref: http://exercism.io/submissions/cc02531fd852476daa6fe1170c8b47c8
+
 const sumOfMultilples = (factors) => (to) => {
-  const ns = Array(to).fill(0)
-  factors.forEach(f => {
-    let n = f
-    while (n < to) {
-      ns[n] = n
-      n += f
-    }
-  })
-  return ns.reduce((s, n) => s + n)
+  return Array.from(Array(to).keys())
+    .filter(n => factors.some(f => n % f === 0))
+    .reduce((s, n) => s + n)
 }
 
 module.exports = fs => ({ to: sumOfMultilples(fs) })

30300b41c577e2cc788759ea9f9aab034d432a03
ryym <ryym.64@gmail.com>
2017-11-07 22:03:10 +0900

    rust: nth-prime: Solve using custom iterator

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,31 +1,32 @@
+// ref: http://exercism.io/submissions/dfc38795d3994b34be862e1bff873ee0
+
+// Iterator::step_by is currently unstable..!
+struct StepBy2(usize);
+
+impl Iterator for StepBy2 {
+    type Item = usize;
+    fn next (&mut self) -> Option<Self::Item> {
+        self.0 += 2;
+        Some(self.0)
+    }
+}
+
 pub fn nth(at: usize) -> Result<usize, &'static str> {
     match at {
-        0 => Err("Invalid nth value"),
+        0 => Err("nth must be greater than 0"),
         1 => Ok(2),
         _ => {
-            let mut i = 1;
-            let mut n = 1;
-            while i < at {
-                n += 2;
-                if is_prime(n) {
-                    i += 1;
-                }
-            }
-            Ok(n)
+            let prime = odds().filter(|&x| is_prime(x)).nth(at - 2);
+            Ok(prime.unwrap())
         }
     }
 }
 
+fn odds() -> StepBy2 {
+    StepBy2(1)
+}
+
 fn is_prime(n: usize) -> bool {
     let end = (n as f64).sqrt() as usize;
-
-    // (3..(end + 1)).step_by(2).all(|x| n % x != 0)
-    let mut i = 3;
-    while i <= end {
-        if n % i == 0 {
-            return false
-        }
-        i += 2;
-    }
-    true
+    odds().take_while(|&x| x <= end).all(|x| n % x != 0)
 }

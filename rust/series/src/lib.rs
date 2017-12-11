cc0bd181465d66949a91cc3216641ac47bedf962
ryym <ryym.64@gmail.com>
2017-12-11 21:36:47 +0900

    rust: series: Write without windows fn

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,11 +1,5 @@
+// No `Vec::windows` version.
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    if len == 0 {
-        return vec!["".to_string(); digits.len() + 1];
-    }
-    digits
-        .chars()
-        .collect::<Vec<_>>()
-        .windows(len)
-        .map(|cs| cs.iter().collect())
-        .collect()
+    let end = if digits.len() < len { 0 } else { digits.len() - len + 1 };
+    (0..end).map(|n| digits[n..n+len].to_string()).collect()
 }

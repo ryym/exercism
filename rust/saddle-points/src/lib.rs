a2634c03aa5bf2df5b478f5c48306d235b1a05c6
ryym <ryym.64@gmail.com>
2017-12-25 07:52:47 +0900

    rust: saddle-points: Simplify logic

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,60 +1,27 @@
-#[derive(Debug)]
-struct Point {
-    min_in_col: bool,
-    max_in_row: bool,
-}
-impl Point {
-    fn is_saddle(&self) -> bool {
-        self.min_in_col && self.max_in_row
-    }
-}
-
-pub fn _find_saddle_points(matrix: &[Vec<u64>]) -> Vec<(usize, usize)> {
+pub fn find_saddle_points(matrix: &[Vec<u64>]) -> Vec<(usize, usize)> {
     if !is_valid_matrix(matrix) {
         panic!("This is not a matrix");
     }
 
     let n_rows = matrix.len();
     let n_cols = matrix[0].len();
-
     if n_cols == 0 {
         return Vec::new();
     }
 
-    let mut min_col_vals = vec![std::u64::MAX; n_cols];
-    let mut points = Vec::with_capacity(n_rows);
+    let max_cols: Vec<u64> = matrix.iter()
+        .map(|row| *row.iter().max().unwrap())
+        .collect();
 
-    for r in 0..n_rows {
-        points.push(Vec::with_capacity(n_cols));
-        let max_val = matrix[r].iter().max();
-
-        for c in 0..n_cols {
-            let val = matrix[r][c];
-            points[r].push(Point {
-                min_in_col: false,
-                max_in_row: match max_val {
-                    Some(max) => val == *max,
-                    None => true,
-                }
-            });
-            if min_col_vals[c] > val {
-                min_col_vals[c] = val;
-            }
-        }
-    }
-
-    for (c, &min) in min_col_vals.iter().enumerate() {
-        for r in 0..n_rows {
-            if min == matrix[r][c] {
-                points[r][c].min_in_col = true;
-            }
-        }
-    }
+    let min_rows: Vec<u64> = (0..n_cols)
+        .map(|c| (0..n_rows).map(|r| matrix[r][c]).min().unwrap())
+        .collect();
 
     let mut saddles = Vec::new();
     for r in 0..n_rows {
         for c in 0..n_cols {
-            if points[r][c].is_saddle() {
+            let v = matrix[r][c];
+            if v == max_cols[r] && v == min_rows[c] {
                 saddles.push((r, c));
             }
         }

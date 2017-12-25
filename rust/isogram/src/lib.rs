f0c9eba93d0d3578697ee4d221c76494fadc1b28
ryym <ryym.64@gmail.com>
2017-12-25 14:40:32 +0900

    rust: isogram: HashSet::insert returns bool

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -2,18 +2,9 @@ use std::collections::HashSet;
 
 pub fn check(s: &str) -> bool {
     let mut seen = HashSet::new();
-    let allowed = vec!['-', ' '];
-
     s.to_string()
         .to_lowercase()
         .chars()
-        .filter(|c| !allowed.contains(c))
-        .all(|c| {
-            if seen.contains(&c) {
-                false
-            } else {
-                seen.insert(c);
-                true
-            }
-        })
+        .filter(|c| c.is_alphabetic())
+        .all(|c| seen.insert(c))
 }
